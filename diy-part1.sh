a7ab8996def9a5ba11b10ace80afac830e8f0eae
hminid <hminid@163.com>
2021-02-16 21:10:19 +0800

    Add helloworld

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add helloworld
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
